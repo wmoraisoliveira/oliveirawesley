7ca02fd7a12c699265ab3b961abcab128401e038
Wesley Oliveira <wesley@lettrlabs.com>
2026-02-05 12:24:23 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.scss'
+
+function App() {
+  return (
+    <main className="home">
+      <img
+        src="/Logo.png"
+        alt="Logo"
+        className="home__logo"
+      />
+      <h1 className="home__title">Oliveira Wesley</h1>
+      <p className="home__subtitle">coming soon</p>
+    </main>
+  )
+}
+
+export default App
