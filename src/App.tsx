dc0841df36efbc8f4769f3f17acda3d22bc7b564
Wesley Oliveira <wesley@lettrlabs.com>
2026-02-06 10:45:18 -0300

    Re-added logo

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import './App.scss'
 function App() {
   return (
     <main className="home">
-      {/* <img
+      <img
         src={`${import.meta.env.BASE_URL}Logo.png`}
-        alt="Logo"
+        alt="Logo - Oliveira Wesley"
         className="home__logo"
-      /> */}
+      />
       <h1 className="home__title">Oliveira Wesley</h1>
       <p className="home__subtitle">coming soon</p>
     </main>
