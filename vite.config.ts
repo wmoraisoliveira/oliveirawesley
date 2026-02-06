eb4fda19d54e924e93f2e8afed3c3759f06ededb
Wesley Oliveira <wesley@lettrlabs.com>
2026-02-06 10:43:35 -0300

    Removed unnecessary comment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Usando domínio customizado (oliveirawesley.com), o site é servido na raiz.
 export default defineConfig({
   base: '/',
   plugins: [react()],
